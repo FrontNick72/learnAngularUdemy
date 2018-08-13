44d5184404eb4703261b11bf46cdeee51efe55c2
Nikita <nekit795@gmail.com>
2018-08-13 22:45:20 +0500

    finish basic things

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -9,6 +9,8 @@ export class ServersComponent implements OnInit {
   allowNewServer = false;
   serverCreationstatus: string = 'No server was created!';
   serverName: string = 'Test server';
+  serverCreated: boolean = false;
+  servers = ['Testserver', 'Testserver 2'];
 
   constructor() {
     setTimeout(() => {
@@ -20,7 +22,14 @@ export class ServersComponent implements OnInit {
   }
 
   onCreateServer() {
+    this.serverCreated = true;
+    this.servers.push(this.serverName);
     this.serverCreationstatus = `Server was created! Name is ${this.serverName}`;
+  }
+
+  inputChanged(event: Event) {
+    const valueInput = (<HTMLInputElement>event.target).value;
+    this.allowNewServer = valueInput ? true : false;
   }
 
   onUpdateServerName(event: Event) {
