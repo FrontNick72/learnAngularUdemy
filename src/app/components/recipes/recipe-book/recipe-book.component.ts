c4e70d7ea5dfb134be5473f97454774a5423c2bb
Демяненко Никита <nd@crtweb.ru>
2018-08-13 17:55:21 +0500

    add recipe list

--- src/app/components/recipes/recipe-book/recipe-book.component.ts
+++ src/app/components/recipes/recipe-book/recipe-book.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Recipes } from '../recipes.module';
 
 @Component({
   selector: 'app-recipe-book',
@@ -6,5 +7,15 @@ import { Component } from '@angular/core';
   styleUrls: ['./recipe-book.component.css']
 })
 export class RecipeBookComponent {
+  recipes: Recipes[] = [
+    new Recipes('A test Recipe', 'This is Simply a test',
+      'http://potchefstroomherald.co.za/wp-content/uploads/sites/117/2015/05/recipe-575434_640_6049519_8594059_6672824.png'),
+    new Recipes('A test Recipe 2', 'This is Difficult a test',
+      'http://potchefstroomherald.co.za/wp-content/uploads/sites/117/2015/05/recipe-575434_640_6049519_8594059_6672824.png'),
+  ];
 
+  constructor() {
+    console.log(this.recipes);
+
+  }
 }
