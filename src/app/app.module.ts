6b3b31735684f76beeb6970175c0d5308bdcc3b1
Демяненко Никита <nd@crtweb.ru>
2018-08-13 12:42:07 +0500

    creating components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,24 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
+import { ShoppingListComponent } from './components/shopping-list/shopping-list.component';
+import { RecipeBookComponent } from './components/recipes/recipe-book/recipe-book.component';
+import { HeaderComponent } from './components/header/header.component';
+import { RecipesComponent } from './components/recipes/recipes.component';
+import { RecipeDetailComponent } from './components/recipes/recipe-detail/recipe-detail.component';
+import { RecipeItemComponent } from './components/recipes/recipe-book/recipe-item/recipe-item.component';
+import { ShoppingEditComponent } from './components/shopping-list/shopping-edit/shopping-edit.component';
 
 @NgModule({
   declarations: [
     AppComponent,
+    ShoppingListComponent,
+    RecipeBookComponent,
+    HeaderComponent,
+    RecipesComponent,
+    RecipeDetailComponent,
+    RecipeItemComponent,
+    ShoppingEditComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/components/shopping-list/shopping-list.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-shop-list',
+  templateUrl: './shopping-list.component.html',
+  styleUrls: ['./shopping-list.component.html']
+})
+export class ShoppingListComponent {
+}
