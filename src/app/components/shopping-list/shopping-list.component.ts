c7d1121e0f31e70da78db6b8294271e18815835a
Nikita <nekit795@gmail.com>
2018-08-14 09:20:40 +0500

    finish basic set up

--- src/app/components/shopping-list/shopping-list.component.ts
+++ src/app/components/shopping-list/shopping-list.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
-
+import { Ingredient } from '../shared/ingredient.model';
 @Component({
   selector: 'app-shop-list',
   templateUrl: './shopping-list.component.html',
   styleUrls: ['./shopping-list.component.html']
 })
 export class ShoppingListComponent {
-  ingredients = [];
+  ingredients: Ingredient[] = [
+    new Ingredient('Apples', 4),
+    new Ingredient('Coconut', 1)
+  ];
 }
