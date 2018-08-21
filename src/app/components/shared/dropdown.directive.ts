e0479d167eafaca845505c029522fea909985b47
Nikita <nekit795@gmail.com>
2018-08-21 09:02:30 +0500

    finish add directive in app

--- src/app/components/shared/dropdown.directive.ts
+++ src/app/components/shared/dropdown.directive.ts
@@ -1,13 +1,17 @@
-import { Directive, ElementRef, Renderer2, OnInit } from '@angular/core';
+import { Directive, ElementRef, Input, HostListener, HostBinding } from '@angular/core';
+import { isComponent } from '@angular/core/src/render3/instructions';
 
 @Directive({
   selector: '[appDropdown]'
 })
-export class DropdownDirective implements OnInit {
-  constructor(private elementRef: ElementRef, private render: Renderer2) {
-    console.log('element Ref', this.elementRef);
+export class DropdownDirective {
+  @HostBinding('class.open') private isOpen: boolean;
+
+  constructor(private elementRef: ElementRef) {
+    this.isOpen = false;
   }
 
-  ngOnInit() {
+  @HostListener('click') onclick() {
+    this.isOpen = !this.isOpen;
   }
 }
