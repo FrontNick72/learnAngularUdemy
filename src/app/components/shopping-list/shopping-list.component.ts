58ab8dc25d63eb117b25eefe74f933d1d46eb789
Демяненко Никита <nd@crtweb.ru>
2018-08-23 10:41:13 +0500

    improve shoplistservice

--- src/app/components/shopping-list/shopping-list.component.ts
+++ src/app/components/shopping-list/shopping-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, OnChanges, DoCheck } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 import { Ingredient } from '../shared/ingredient.model';
 import { ShoppingListService } from './shopping-list.service';
@@ -9,20 +9,18 @@ import { ShoppingListService } from './shopping-list.service';
   styleUrls: ['./shopping-list.component.html'],
   providers: [ShoppingListService]
 })
-export class ShoppingListComponent implements OnInit, DoCheck {
+export class ShoppingListComponent implements OnInit {
   ingredients: Ingredient[];
-
-  newIngredient: string;
-  newAmountIngredient: string;
 
   constructor(private shoppingListService: ShoppingListService) {}
 
   ngOnInit(): void {
     this.ingredients = this.shoppingListService.getIngredients();
-  }
-
-  ngDoCheck() {
-    this.ingredients = this.shoppingListService.getIngredients();
+    this.shoppingListService.ingredientAdded.subscribe(
+      (ingredients: Ingredient[]) => {
+        this.ingredients = ingredients;
+      }
+    );
   }
 
 }
--- src/app/components/shopping-list/shopping-list.service.ts
+++ src/app/components/shopping-list/shopping-list.service.ts
@@ -3,7 +3,7 @@ import { EventEmitter } from '@angular/core';
 import { Ingredient } from '../shared/ingredient.model';
 
 export class ShoppingListService {
-  ingredientAdded = new EventEmitter<Ingredient>();
+  ingredientAdded = new EventEmitter<Ingredient[]>();
 
   private ingredients: Ingredient[] = [
     new Ingredient('Apples', 4),
@@ -16,9 +16,11 @@ export class ShoppingListService {
 
   addIngredient(ingredient: Ingredient) {
     this.ingredients.push(ingredient);
+    this.ingredientAdded.emit(this.ingredients.slice());
   }
 
   clear() {
     this.ingredients.splice(0, this.ingredients.length);
+    this.ingredientAdded.emit(this.ingredients.slice());
   }
 }
