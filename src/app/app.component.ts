d379c260ed4b14c5e74fc373472d363100299de6
Nikita <nekit795@gmail.com>
2018-09-13 08:19:16 +0500

    start http

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,12 +4,28 @@ import { Component } from '@angular/core';
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
-  // styles: [`
-  //   h3 {
-  //     color: darkblue;
-  //   }
-  // `]
 })
 export class AppComponent {
-  name = 'Nikita';
+  servers = [
+    {
+      name: 'Testserver',
+      capacity: 10,
+      id: this.generateId()
+    },
+    {
+      name: 'Liveserver',
+      capacity: 100,
+      id: this.generateId()
+    }
+  ];
+  onAddServer(name: string) {
+    this.servers.push({
+      name: name,
+      capacity: 50,
+      id: this.generateId()
+    });
+  }
+  private generateId() {
+    return Math.round(Math.random() * 10000);
+  }
 }
