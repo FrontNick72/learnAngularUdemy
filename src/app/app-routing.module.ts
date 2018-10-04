8f53078cef4267739670c609a4a305c5e43e936e
Nikita <nekit795@gmail.com>
2018-10-04 08:10:12 +0500

    add auth module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,14 +2,11 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 import { HomeComponent } from './home-component/home.component';
-import { SignupComponent } from './components/auth/signup/signup.component';
-import { SigninComponent } from './components/auth/signin/signin.component';
+
 import { AuthGuard } from './components/auth/auth-guard.service';
 
 const appRoutes: Routes = [
-  { path: '', redirectTo: '/recipes', pathMatch: 'full' },
-  { path: 'signup', component: SignupComponent },
-  { path: 'signin', component: SigninComponent },
+  { path: '', redirectTo: '/recipes', pathMatch: 'full' }
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,5 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { ShoppingListComponent } from './components/shopping-list/shopping-list.component';
@@ -12,28 +11,25 @@ import { RecipeService } from './components/recipes/recipe.service';
 import { HttpModule } from '@angular/http';
 import { DataStorageService } from './components/shared/data-storage.service';
 import { AuthService } from './components/auth/auth.service';
-import { SignupComponent } from './components/auth/signup/signup.component';
-import { SigninComponent } from './components/auth/signin/signin.component';
 import { AuthGuard } from './components/auth/auth-guard.service';
 import { RecipesModule } from './components/recipes/recipes.module';
 import { SharedModule } from './components/shared/shared.module';
 import { ShoppingListModule } from './components/shopping-list/shopping-list.module';
+import { AuthModule } from './components/auth/auth.module';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     HomeComponent,
-    SignupComponent,
-    SigninComponent
   ],
   imports: [
     BrowserModule,
-    FormsModule,
     HttpModule,
     RecipesModule,
     SharedModule,
     ShoppingListModule,
+    AuthModule,
     AppRoutingModule,
   ],
   providers: [ShoppingListService, RecipeService, DataStorageService, AuthService, AuthGuard],
